95d31d818175bacd331ffb2a8056a6da05348b28
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2025-03-15 21:53:34 +0200

    Add files via upload

---
+++ Homework-1 14.3.2025/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_homework
+{
+     class Employee : Person
+    {
+        
+
+        public Employee(string name,int age,string gender,string phone) :base(name,age,gender,phone)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phone = phone;
+
+        }
+    }
+}
---
+++ Homework-1 14.3.2025/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_homework
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string gender;
+        public string phone;
+
+        public Person(string name,int age ,string gender,string phone) 
+        { 
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phone = phone;
+        }
+
+
+    }
+
+   
+}
